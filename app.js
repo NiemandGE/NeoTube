bba01b3c3e24af5c9e31072b77682c64ba99f927
Niemand <yamahatvt@gmail.com>
2026-04-20 18:29:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ import { getFirestore, collection, addDoc, onSnapshot, query, orderBy } from "ht
 // --- КОНФИГУРАЦИЯ FIREBASE ---
 // ЗАМЕНИТЕ ЭТОТ ОБЪЕКТ НА ДАННЫЕ ИЗ ВАШЕЙ КОНСОЛИ FIREBASE
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAvjmo78uTKkCY96uzTiT4boDeCVoEJhOk",
+  authDomain: "epsilonbase-316d2.firebaseapp.com",
+  projectId: "epsilonbase-316d2",
+  storageBucket: "epsilonbase-316d2.firebasestorage.app",
+  messagingSenderId: "230539197818",
+  appId: "1:230539197818:web:7ddae53c499b318f6caac3"
 };
 
 // Инициализация
